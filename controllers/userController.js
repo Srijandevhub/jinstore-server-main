e31304bf529f20bda74b40c4f3f7f657564ca4c0
Your Name <s.r.j.s.n2017@gmail.com>
2025-01-06 19:59:36 +0530

    rc

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,8 +39,7 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         const { identifier, password, rememberme } = req.body;
-        //console.log(identifier, password, rememberme);
-        const user = await User.findOne({ $or: [ { username: identifier }, { email: identifier } ] });
+        const user = await User.findOne({ $or: [ { username: identifier }, { email: identifier } ] }).select("-shippingdetailsids	-orderids -cartid -wishlistid -billingaddressid");
         if (!user) {
             return res.status(400).json({ message: "User not found" });
         }
@@ -54,7 +53,8 @@ const loginUser = async (req, res) => {
         if (rememberme) {
             res.cookie("jinstorerefresh", refreshtoken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
         }
-        res.status(200).json({ message: "User loggedin" });
+        user.password = "";
+        res.status(200).json({ message: "User loggedin", user: user });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message });
     }
